0ea9046d3315c67aff7ac20edd9fc2c742f91d4e
Julia Kester <kester@oswego.edu>
2021-12-01 00:14:15 -0500

    Day 1 complete

--- Day1.js
+++ Day1.js
@@ -5,4 +5,28 @@ fs.readFile('Day1.txt', 'utf8', function (err, data) {
 	}
 	let lines = data.split('\n').map(Number);
 	let length = lines.length;
+
+    let increases = 0;
+    lines.forEach((depth, i) => {
+        if (i > 0 && depth > lines[i-1]) {
+            increases++;
+        }
+    });
+
+    let prev = 0;
+    let increases2 = 0;
+    const maxIndex = length - 2;
+    lines.forEach((depth, i) => {
+        if (i < maxIndex) {
+            let newVal;
+            newVal = depth + lines[i +1] + lines[i+2];
+            if (i > 0 && newVal > prev) {
+                increases2 ++;
+            }
+            prev = newVal;
+        }
+    });
+
+    console.log("P1:", increases);
+    console.log("P2:", increases2)
 });
