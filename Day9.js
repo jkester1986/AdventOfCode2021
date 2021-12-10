0ce0df33899e4dd7fa54b7286ecfa2beac083c9d
Julia Kester <kester@oswego.edu>
2021-12-09 20:04:02 -0500

    Day 9 complete

--- Day9.js
+++ Day9.js
@@ -9,30 +9,72 @@ fs.readFile('Day9.txt', 'utf8', function (err, data) {
 
     let lowPointCount = 0;
     let lowestPointsTotal = 0;
+    let lowPoints = {}
 
-    lines.forEach((line, i) => {
-        for(let smoke = 0; smoke < lineLength; smoke++) {
+    lines.forEach((line, y) => {
+        for(let x = 0; x < lineLength; x++) {
             let lowest = true;
-            const currentHeight = line[smoke];
+            const currentHeight = line[x];
             if (
                 // not first line, check against val in previous line
-                (i !== 0 && lines[i-1][smoke] <= currentHeight) ||
+                (y !== 0 && lines[y-1][x] <= currentHeight) ||
                 // not last line, check against val in next line
-                (i !== lastInd && lines[i+1][smoke] <= currentHeight) ||
+                (y !== lastInd && lines[y+1][x] <= currentHeight) ||
                 // not first val, check against prev val
-                (smoke !== 0 && line[smoke-1] <= currentHeight) ||
+                (x !== 0 && line[x-1] <= currentHeight) ||
                 // not last val, check against next val
-                (smoke !== lineLength - 1 && line[smoke+1] <= currentHeight)
+                (x !== lineLength - 1 && line[x+1] <= currentHeight)
             ) {
                 lowest = false;
             }
             if(lowest) {
                 lowPointCount++;
                 lowestPointsTotal += currentHeight;
+                lowPoints[`${x},${y}`] = {
+                    x,
+                    y,
+                    depth: currentHeight
+                };
             }
         }
     });
 
     console.log("P1:", lowestPointsTotal + lowPointCount)
+
+    let foundCoords = new Set();
+
+    function findBasinSize(depth, x, y) {
+        let coords = `${x},${y}`;
+        if(depth === undefined || depth === 9 || foundCoords.has(coords)) {
+            return 0;
+        }
+
+        foundCoords.add(coords);
+
+        const plusX = x+1;
+        const plusY = y+1;
+        const minusX = x-1;
+        const minusY = y-1;
+
+
+        return depth + findBasinSize(lines[plusY]?.[x], x, plusY) +
+            findBasinSize(lines[minusY]?.[x], x, minusY) +
+            findBasinSize(lines[y]?.[minusX], minusX, y) +
+            findBasinSize(lines[y]?.[plusX], plusX, y)
+    }
+
+    let depths = []
+    Object.entries(lowPoints).forEach(([key, val]) => {
+        const { x, y, depth } = val;
+
+        findBasinSize(depth, x, y);
+        const basinSize = foundCoords.size;
+        depths.push(basinSize);
+        foundCoords.clear()
+    });
+
+    const last3 = depths.sort((a, b) => a - b).slice(depths.length - 3);
+
+    console.log("P2:", last3[0] * last3[1] * last3[2]);
 
 });
